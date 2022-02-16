ac68fbe718fc2cec965c6c3034d64d11fc384809
Jacob Danielsson <danielsson_jacob@yahoo.com>
2022-02-16 16:03:12 +0100

    feat: add Std Class with print and println method

---
+++ src/main/java/Std.java
@@ -0,0 +1,20 @@
+public class Std {
+    public static void print(String toPrint){
+        System.out.print(toPrint);
+    }
+    public static void print(Long toPrint){
+        System.out.print(toPrint);
+    }
+    public static void print(double toPrint){
+        System.out.print(toPrint);
+    }
+    public static void println(String toPrint){
+        System.out.println(toPrint);
+    }
+    public static void println(long toPrint){
+        System.out.println(toPrint);
+    }
+    public static void println(double toPrint){
+        System.out.println(toPrint);
+    }
+}
